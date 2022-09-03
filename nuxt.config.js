d0dda9188b5d908ec14717f1ab1469a4e195ab83
danushka96 <danushkaherath96@gmail.com>
2022-09-03 16:35:56 +0530

    added build configurations

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - portfolio',
-    title: 'portfolio',
+    titleTemplate: '%s - Software Engineer',
+    title: 'Danushka Hearth',
     htmlAttrs: {
       lang: 'en'
     },
